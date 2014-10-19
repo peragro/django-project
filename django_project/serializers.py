58b6c5e4220fd908e46c0a9287bbb5599c316b25
sueastside <sueastside@gmail.com>
2014-10-19 22:07:46 +0200

    Added HyperlinkedRelatedMethod

--- django_project/serializers.py
+++ django_project/serializers.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 from rest_framework.relations import HyperlinkedRelatedField
+from rest_framework.relations import RelatedField
 
 from notifications.models import Notification
 from follow.models import Follow
@@ -25,36 +26,55 @@ class SerializerMethodFieldArgs(serializers.Field):
     def field_to_native(self, obj, field_name):
         value = getattr(self.parent, self.method_name)(obj, *self.args)
         return self.to_native(value)
-        
+
+
+class HyperlinkedRelatedMethod(RelatedField):
+    """
+    A field that replaces usage of:
+    class A(GenericForeignKeyMixin):
+      field = SerializerMethodFieldArgs('get_related_object_url', 'field')
+    """
+    def __init__(self):
+        super(HyperlinkedRelatedMethod, self).__init__()
+
+    def field_to_native(self, obj, field_name):
+        value = GenericForeignKeyMixin.get_related_object_url(self.parent, obj, field_name)
+        return self.to_native(value)
+
 
 class GenericForeignKeyMixin(object):
     def get_related_object_url(self, obj, field):
         try:
             obj = getattr(obj, field)
+            if callable(obj):
+              obj = obj()
             default_view_name = '%(model_name)s-detail'
-            
+
             format_kwargs = {
                 'app_label': obj._meta.app_label,
                 'model_name': obj._meta.object_name.lower()
             }
             view_name = default_view_name % format_kwargs
-            print('get_related_object_url::view_name', view_name)
+            print('*get_related_object_url::view_name', view_name)
             s = serializers.HyperlinkedIdentityField(source=obj, view_name=view_name)
             s.initialize(self, None)
             return s.field_to_native(obj, None)
         except Exception as e:
-            print(e)
-            return ''
-            
+            print('WARN', e)
+            return None
+
 
 class ExtendedHyperlinkedModelSerializer(serializers.HyperlinkedModelSerializer):
     def to_native(self, obj):
-        from rest_framework.relations import RelatedField, PrimaryKeyRelatedField
         res = super(ExtendedHyperlinkedModelSerializer, self).to_native(obj)
         if obj:
             res['id'] = obj.serializable_value('pk')
             for field_name, field in self.fields.items():
-                if isinstance(field , RelatedField):
+                if isinstance(field , HyperlinkedRelatedMethod):
+                  serializable_value = obj.serializable_value(field_name)()
+                  res[field_name+"_id"] = serializable_value.pk if serializable_value else None
+                  res[field_name+"_descr"] = str(serializable_value) if serializable_value else None
+                elif isinstance(field , RelatedField):
                     serializable_value = obj.serializable_value(field_name)
                     if isinstance(serializable_value, int):
                         res[field_name+"_id"] = obj.serializable_value(field_name)
@@ -62,8 +82,10 @@ class ExtendedHyperlinkedModelSerializer(serializers.HyperlinkedModelSerializer)
                     elif serializable_value == None:
                         res[field_name+"_id"] = None
                         res[field_name+"_descr"] = None
+                else:
+                  print field_name, field.__class__
         return res
-        
+
 
 class FollowSerializerMixin(object):
     def to_native(self, obj):
@@ -79,7 +101,7 @@ class FollowSerializer(serializers.Serializer):
     def to_native(self, obj):
         def reverse_url(result):
             return reverse('%s-detail'%result.target._meta.object_name.lower(), args=[result.target.id])
-            
+
         ret = {'url': reverse_url(obj), 'type': obj.target._meta.object_name, '__str__': str(obj.target) }
         return ret
 
@@ -101,8 +123,8 @@ class UserNameSerializer(ExtendedHyperlinkedModelSerializer):
     name = serializers.CharField(source='username', read_only=True)
     class Meta:
         model = User
-        fields = ('id', 'url', 'name')        
-        
+        fields = ('id', 'url', 'name')
+
 
 class MilestoneSerializer(ExtendedHyperlinkedModelSerializer):
     class Meta:
@@ -142,42 +164,42 @@ class StatusSerializer(ExtendedHyperlinkedModelSerializer):
     class Meta:
         model = models.Status
 
-        
+
 class ObjectTaskSerializer(GenericForeignKeyMixin, serializers.Serializer):
     content_object = SerializerMethodFieldArgs('get_related_object_url', 'content_object')
     content_object_descr = serializers.CharField(source='content_object', read_only=True)
-            
-               
+
+
 class TaskSerializer(FollowSerializerMixin, ExtendedHyperlinkedModelSerializer):
     class Meta:
         model = Task
-        read_only_fields = ('author', 'project') 
-        
+        read_only_fields = ('author', 'project')
+
     def save_object(self, task, *args, **kwargs):
         task.save_revision(self.context['request'].user, task.description, *args, **kwargs) #TODO: add interesting commit message!
-        
-    
+
+
 class NotificationSerializer(GenericForeignKeyMixin, serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     level = serializers.CharField()
-    
+
     recipient_descr = serializers.CharField(source='recipient', read_only=True)
     recipient = SerializerMethodFieldArgs('get_related_object_url', 'recipient')
-    
+
     actor_descr = serializers.CharField(source='actor', read_only=True)
     actor = SerializerMethodFieldArgs('get_related_object_url', 'actor')
-    
+
     verb = serializers.CharField()
     description = serializers.CharField()
-    
+
     target_descr = serializers.CharField(source='target', read_only=True)
     target = SerializerMethodFieldArgs('get_related_object_url', 'target')
-    
+
     action_object_descr = serializers.CharField(source='action_object', read_only=True)
     action_object = SerializerMethodFieldArgs('get_related_object_url', 'action_object')
-    
+
     timesince = serializers.CharField()
-    
+
     __str__ = serializers.CharField()
 
 
@@ -207,16 +229,16 @@ class VersionSerializer(serializers.Serializer):
         ver['object'] = version.field_dict
         ver['m2m_data'] = version.object_version.m2m_data
         return ver
-        
-        
+
+
 class CommentSerializer(GenericForeignKeyMixin, ExtendedHyperlinkedModelSerializer):
     content_object = SerializerMethodFieldArgs('get_related_object_url', 'content_object')
     content_object_descr = serializers.CharField(source='content_object', read_only=True)
-    
+
     class Meta:
         model = Comment
         exclude = ('content_type', 'object_pk', )
-        
+
     def get_parent_object(self, instance=None):
         if instance:
             return instance.content_object
@@ -227,12 +249,12 @@ class CommentSerializer(GenericForeignKeyMixin, ExtendedHyperlinkedModelSerializ
             parent_viewset = resolve(path)
             object = parent_viewset.func.cls.queryset.get(**parent_viewset.kwargs)
             return object
-        
+
     def restore_object(self, attrs, instance=None):
-        #assert instance is None, 'Cannot update comment with CommentSerializer'      
-        
-        object = self.get_parent_object(instance)   
-        values = {'comment': attrs['comment'], 'content_object':object, 'author':self.context['request'].user}                       
+        #assert instance is None, 'Cannot update comment with CommentSerializer'
+
+        object = self.get_parent_object(instance)
+        values = {'comment': attrs['comment'], 'content_object':object, 'author':self.context['request'].user}
         if instance:
             for (key, value) in values.items():
                 setattr(instance, key, value)
@@ -240,4 +262,3 @@ class CommentSerializer(GenericForeignKeyMixin, ExtendedHyperlinkedModelSerializ
         else:
             comment = Comment(**values)
             return comment
-        
