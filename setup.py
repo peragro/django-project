40e56eee4b335fee6e39e9375e867f0c063c52f0
sueastside <sueastside@gmail.com>
2016-04-21 17:09:10 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Django app for project management',
+    'author': 'sueastside',
+    'url': 'https://github.com/sueastside/django-project',
+    'download_url': 'https://github.com/sueastside/django-project',
+    'author_email': 'No, thanks',
+    'version': '0.1',
+    'test_suite': 'tests.suite',
+    'install_requires': [],
+    'test_requires': [],
+    'packages': ['django_project'],
+    'scripts': [],
+    'name': 'django-project',
+}
+
+setup(**config)
