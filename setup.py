9547fa64f20d8280788f5e5ce4aa9f0aeda9e9b2
sueastside <sueastside@gmail.com>
2016-04-21 17:03:47 +0200

    Remove deps from setup.py
    as they are contained in requirements.txt

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
     'author_email': 'No, thanks',
     'version': '0.1',
     'test_suite': 'tests.suite',
-    'install_requires': ['django==1.6.7', 'djangorestframework==2.3.14', 'django-autoslug==1.7.2', 'django-reversion==1.8.1', 'south==1.0', 'pytz==2014.4', 'django-notifications-hq==0.8.0', 'django-follow==0.6.1', 'django-filter==0.7', 'drf-extensions==0.2.5', 'django-cors-headers==0.12', 'django-smart-selects==1.0.9'],
+    'install_requires': [],
     'test_requires': [],
     'packages': ['django_project'],
     'scripts': [],
